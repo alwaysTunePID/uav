7c73cdf6e04a9348b62c7e9d6237352a4b6fe7e0
Jesper Carlsson <jesperjepplercarlsson@gmail.com>
2019-08-05 14:54:47 +0200

    Renamed input_thread to io_thread and cleaned up code.

--- uav/io_thread.c
+++ uav/io_thread.c
@@ -5,44 +5,31 @@
 
 static int input_thread_ret_val;
 
-void set_K(inputs_t *p, int val)
-{
+/* void set_K(inputs_t *p, int val) {
     pthread_mutex_lock(&(p->mutex));
     p->K = val;
     pthread_mutex_unlock(&(p->mutex));
-}
+} */
 
+int input_main(inputs_t *inputs) {
+	double K;
 
-int input_main(inputs_t *inputs){
-
-    double K;
-    double G;
-
-    while (rc_get_state() != EXITING)
-    {
-        
-        
-        K=rc_dsm_ch_normalized(8);
+    while (rc_get_state() != EXITING) {
         //scanf(" %lf", &K);
         //printf("You passed the first scanf\n");
-        //scanf(" %lf", &G);
-        
-        //printf("You passed the second scanf\n");
-    
-        set_K(inputs, K);
+        //set_K(inputs, K);
         //fflush(stdin);
-        printf("K is %lf", K);
+        //printf("K is %lf", K);
         rc_usleep(500000);
     }
-    return 0;
 
+    return 0;
 }
 
 
-void *input_thread_func(void *arg){
+void *input_thread_func(void *arg) {
     input_thread_ret_val = input_main((inputs_t*)arg);
-    if (input_thread_ret_val ==-1)
-    {
+    if (input_thread_ret_val ==-1) {
         rc_set_state(EXITING);
     }
 	
