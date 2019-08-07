cca38286e897befcb94baf7ba106f74d4c57e85b
Jesper Carlsson <jesperjepplercarlsson@gmail.com>
2019-08-07 13:38:45 +0200

    Added queue.

---
+++ uav/queue.h
@@ -0,0 +1,24 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+typedef union Data {
+    int d;
+    double lf;
+    char str[255];
+} Data;
+
+typedef struct Queue {
+    unsigned int size;
+    unsigned int head;
+    unsigned int trail;
+    char q[10][255];
+    void* overfull;
+} Queue;
+
+void init(Queue* q);
+void push(Queue* q, char element[]);
+void pop(Queue* q, char* output);
+int empty(Queue* q);
+int full(Queue* q);
+
+#endif;
