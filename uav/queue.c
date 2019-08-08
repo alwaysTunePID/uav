affb5b7cf6b553b60a8e346af597be948a0a9678
Jesper Carlsson <jesperjepplercarlsson@gmail.com>
2019-08-08 08:33:10 +0200

    Added dynamic message array for queue

--- uav/queue.c
+++ uav/queue.c
@@ -3,18 +3,23 @@
 #include <stdlib.h>
 
 void queue_init(Queue* q, unsigned int message_size) {
-    q->message_size = message_size;
+    q->size = message_size;
     q->messages=malloc(message_size*sizeof(q->message));
     q->head = q->trail = 0;
     //realloc(&(q->q), q->size * sizeof(q->q));
 }
 
-void queue_push(Queue* q, char element[]) {
-    strcpy(q->q[q->trail++ % q->size], element);
+
+void increase_message_size(char** messages, int size) {
+	realloc(messages, 10 * sizeof(*messages));
+}
+
+void queue_push(Queue* q, char* element) {
+	messages+(q->trail++ %q->size) = element
 }
 
 void queue_pop(Queue* q, char* output) {
-    strcpy(output, q->q[q->head++ % q->size]);
+	strcpy(output, *(messages + (q->head++ % q->size)));
 }
 
 int queue_empty(Queue* q) {
