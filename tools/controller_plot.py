42cac9bf1b85c8688cf1ebadac717aa844c3414a
Jesper Carlsson <jesperjepplercarlsson@gmail.com>
2019-08-09 11:06:56 +0200

    added support for multiple plots in the python script

--- tools/controller_plot.py
+++ tools/controller_plot.py
@@ -2,49 +2,66 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data_label = input("data:")
-file_name = f"controller_{data_label}.log"
-
-# Open file_name safely and store data as a matrix
-with open(file_name) as file:
-    data  = np.loadtxt(file, delimiter=' ')
-
-# This should be changed
+nbr_of_plots = data_label.split(" ")
+print(nbr_of_plots)
+file_names = []
 ts = 1/200
-length = data.shape[0]
-time = np.transpose(ts*np.arange(0, length))
+for data_file in nbr_of_plots:
+    file_names.append(f"controller_{data_file}.log")
 
-#print(time)
-#print(data)
+
 font = {'family': 'serif',
         'color':  'darkred',
         'weight': 'normal',
         'size': 16,
         }
 
-counter = 0
-loop_length = int(data.shape[1])
-print(loop_length)
+data_labels = {
+        'imu_data': ['roll angle', 'pitch angle', 'yaw angle',
+                'roll rate', 'pitch rate', 'yaw rate'],
+        'motor': ['v1', 'v2', 'v3', 'v4'],
+        'errors': ['roll angle error', 'pitch angle error',
+                'roll rate error', 'pitch rate error', 'yaw rate error'],
+        'PID': ['k roll angle', 'k pitch angle',
+            'roll rate pid', 'pitch rate pid', 'yaw rate pid'],
+        'references': ['roll rate ref', 'pitch rate ref', 'yaw rate ref'],
+    }
+
+# Open file_name safely and store data as a matrix
+file_nbr = 0
+for file_name in file_names:
+    with open(file_name) as file:
+        data  = np.loadtxt(file, delimiter=' ')
+    length = data.shape[0]
+    time = np.transpose(ts*np.arange(0, length))
+    counter = 0
+    loop_length = int(data.shape[1])
+    for i in range(loop_length):
+        plt.figure()
+        plt.plot(time, data[:,counter],'b')
+        plt.title(data_labels[nbr_of_plots[file_nbr]][counter], fontdict=font)
+        counter +=1
+
+    file_nbr +=1
+
+# This should be changed
+
+
+
+#print(time)
+#print(data)
+
+
+
+#print(loop_length)
 # labels = ['u1 v1','u2 v2',
 #         'u3 v3','u4 v4',
 #         'pitch roll', 'Ppitch Proll',
 #         'I_a_p I_a_r', 'K-del rate loop'
 #         ]
-data_labels = {
-        'imu_data': ['roll angle', 'pitch angle', 'yaw angle',
-                        'roll rate', 'pitch rate', 'yaw rate'],
-        'motor': ['v1', 'v2', 'v3', 'v4'],
-        'errors': ['roll angle error', 'pitch angle error',
-                'roll rate error', 'pitch rate error', 'yaw rate error'],
-        'PID': ['k roll angle', 'k pitch angle',
-                'roll rate pid', 'pitch rate pid', 'yaw rate pid'],
-        'references': ['roll rate ref', 'pitch rate ref', 'yaw rate ref'],
-        }
 
-for i in range(loop_length):
-    plt.figure()
-    plt.plot(time, data[:,counter],'b')
-    plt.title(data_labels[data_label][counter], fontdict=font)
-    counter +=1
+
+
 #     plt.plot(time, data[:,counter],'orange')
 #     counter+=1
     
