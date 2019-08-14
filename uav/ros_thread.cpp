0622af539d78033d9d1ea419107955bfcc0387c9
Emil <kemil.frid@gmail.com>
2019-08-14 08:43:59 +0200

    A small example of ROS communication.

--- uav/ros_thread.cpp
+++ uav/ros_thread.cpp
@@ -1,12 +1,31 @@
 #include "ros_thread.h"
 #include <robotcontrol.h>
+#include <ros/ros.h>
 
 static int ros_thread_ret_val;
 
-void* ros_thread_func() {
+int ros_main() {
+    // Announce this program to the ROS master as a "node" called "hello_world_node"
+    ros::init(argc, argv, "hello_world_node");
+    // Start the node resource managers (communication, time, etc)
+    ros::start(); // Broadcast a simple log message ROS_INFO_STREAM("Hello, world!");
     while(rc_get_state() != EXITING) {
-        //Do stuff
-    }
 
-    return (void*) ros_thread_ret_val;
+        // Process ROS callbacks until receiving a SIGINT (ctrl-c)
+        ros::spin();
+        
+    }
+    // Stop the node's resources
+    ros::shutdown(); 
+    return 0;
+}
+
+void *ros_thread_func(){
+    ros_thread_ret_val = ros_main();
+    if (ros_thread_ret_val ==-1)
+    {
+        rc_set_state(EXITING);
+    }
+	
+    return (void*)&ros_thread_ret_val;
 }
